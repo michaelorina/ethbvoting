6736557a161617df63a38298c408b765788a0c80
michaelorina <orinamichael4@gmail.com>
2023-08-07 16:55:53 +0300

    .

--- src/hocs/with-auth-guard.js
+++ src/hocs/with-auth-guard.js
@@ -3,5 +3,6 @@ import { AuthGuard } from 'src/guards/auth-guard';
 export const withAuthGuard = (Component) => (props) => (
   <AuthGuard>
     <Component {...props} />
+    App.displayName = 'ethbvoting';
   </AuthGuard>
 );
